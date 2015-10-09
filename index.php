81a008d035a5ff068816ed26cd978418607915d8
dilbadil <abdillah308@gmail.com>
2015-10-09 12:58:41 +0700

    test 2

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-echo "hello boss";exit;
 session_start();
 require_once "config.php";
 require_once('lib/ganon.php');
+echo "hello boss";exit;
 
 // if (isset($_SESSION['user_id'])) {
 // 	if ($_SESSION['user_id'] != '1889885564') {
